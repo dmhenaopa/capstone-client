a3dca1c2dc2160fbfb71a573e458478aa4417c8f
dmhenaopa <dimhenaopa@gmail.com>
2022-03-11 22:37:57 -0500

    :construction: Add email send service with EmailJS and custom alerts with SweetAlert

--- src/components/Forms/FormContact/FormContact.js
+++ src/components/Forms/FormContact/FormContact.js
@@ -1,39 +1,145 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { Fragment } from 'react';
+import { Formik } from 'formik';
+import emailjs from '@emailjs/browser';
+import swal from 'sweetalert';
 
 export function FormContact() {
-  const { register, handleSubmit } = useForm();
-  function onSubmit(values) {
-    // Values para enviar la información
+  function SendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_p2xy1it', 'template_oig4wcr', e.target, '7d3yZ-_aCiGKZ6AMe').then((result) => {
+      swal('Confirmación de envio', '¡Muchas gracias! Pronto estaremos en contacto con usted.', 'success');
+    }, (error) => {
+      console.log(error.text);
+    });
   }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor='firstname'>Nombres</label>
-      <input type='text' {...register('firstname')} required />
+    <Fragment>
+      <Formik
+        initialValues={{
+          firstname:'',
+          lastname:'',
+          email:'',
+          telephone:'',
+          description:''
+        }}
+        validate={(values) => {
+          let errors = {};
+          if(!values.firstname){
+            errors.firstname = 'Por favor, ingrese su nombre';
+          } else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.firstname)){
+            errors.firstname = 'El nombre solo puede contener letras y espacios';
+          }
+          if(!values.lastname){
+            errors.lastname = 'Por favor, ingrese sus apellidos';
+          } else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.lastname)) {
+            errors.lastname = 'Los apellidos solo pueden contener letras y espacios';
+          }
+          if(!values.email){
+            errors.email = 'Por favor, ingrese su correo electrónico';
+          } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)) {
+            errors.email = 'Ingrese un correo electrónico válido';
+          }
+          if(!values.telephone){
+            errors.telephone = 'Por favor, ingrese su télefono de contacto';
+          } else if(!/^[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/.test(values.telephone)) {
+            errors.telephone = 'El número telefónico solo puede contener números y guiones';
+          }
+          if(values.subject === ''){
+            errors.subject = 'Por favor, seleccione una de las opciones del menú';
+          }
+          if(!values.description){
+            errors.description = 'Por favor, ingrese un texto que describa su solicitud';
+          }
+          return errors;
+        }} 
+      >
 
-      <label htmlFor='lastname'>Apellidos</label>
-      <input type='text' {...register('lastname')} required />
-      
-      <label htmlFor='email'>Correo electrónico</label>
-      <input type='email' {...register('email')} required />
+        {( {values, errors, touched, handleChange, handleBlur} ) => (
+          <form id='form' onSubmit={SendEmail}>
+            <div>
+              <label htmlFor="firstname">Nombre</label>
+              <input 
+                type="text" 
+                id="firstname" 
+                name="firstname" 
+                value={values.firstname}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {touched.firstname && errors.firstname && <div className='error'>{errors.firstname}</div>}
+            </div>
 
-      <label htmlFor='phone'>Teléfono</label>
-      <input type='tel' {...register('phone')} required />
+            <div>
+              <label htmlFor="lastname">Apellidos</label>
+              <input 
+                type="text" 
+                id="lastname" 
+                name="lastname" 
+                value={values.lastname}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {touched.lastname && errors.lastname && <div className='error'>{errors.lastname}</div>}
+            </div>
 
-      <label htmlFor='subject'>Asunto</label>
-      <select {...register('subject')} required>
-          <option></option>
-          <option value='buy'>Compra de productos</option>
-          <option value='update'>Actualización de datos</option>
-          <option value='information'>Información productos</option>
-          <option value='tech-support'>Soporte o fallas técnicas</option>
-          <option value='other'>Otro</option>
-      </select>
+            <div>
+              <label htmlFor="email">Correo electrónico</label>
+              <input 
+                type="email" 
+                id="email" 
+                name="email"
+                placeholder='usuario@correo.com'
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {touched.email && errors.email && <div className='error'>{errors.email}</div>}
+            </div>
 
-      <label htmlFor='description'>Descripción</label>
-      <textarea {...register('description')} required></textarea>
-      <button type="submit">Contactar a ventas</button>
-    </form>
+            <div>
+              <label htmlFor="telephone">Número telefónico</label>
+              <input
+                type="text"
+                id="telephone"
+                name="telephone"
+                placeholder='300-000-0000 o 300000000'
+                value={values.telephone}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {touched.telephone && errors.telephone && <div className='error'>{errors.telephone}</div>}
+            </div>
+
+            <div>
+              <label htmlFor='subject'>Asunto</label>
+              <select required id='subject' name='subject'>
+                <option></option>
+                <option value='compra'>Compra de productos</option>
+                <option value='actualización'>Actualización de datos</option>
+                <option value='información'>Información productos</option>
+                <option value='soporte-tech'>Soporte o fallas técnicas</option>
+                <option value='otro'>Otro</option>
+              </select>
+              {touched.subject && errors.subject && <div className='error'>{errors.subject}</div>}
+            </div>
+
+            <div>
+              <label htmlFor='description'>Descripción</label>
+              <textarea
+                required
+                id='description'
+                name='description'
+                placeholder='Ingrese un texto que describa su solicitud'>
+              </textarea>
+              {touched.description && errors.description && <div className='error'>{errors.description}</div>}
+            </div>
+
+            <button type="submit">Enviar formulario</button>
+          </form>
+        )}
+      </Formik>
+    </Fragment>
   );
 }
-  
--- src/components/Forms/FormHome/FormHome.js
+++ src/components/Forms/FormHome/FormHome.js
@@ -1,15 +1,14 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { Formik } from 'formik';
 import emailjs from '@emailjs/browser';
+import swal from 'sweetalert';
 
 export function FormHome() {
-  const [sendForm, setSendForm] = useState(false);
-
   function SendEmail(e) {
     e.preventDefault();
 
     emailjs.sendForm('service_p2xy1it', 'template_6o7eivc', e.target, '7d3yZ-_aCiGKZ6AMe').then((result) => {
-      alert('Confirmación de envio');
+      swal('Confirmación de envio', '¡Muchas gracias! Pronto estaremos en contacto con usted.', 'success');
     }, (error) => {
       console.log(error.text);
     });
@@ -43,23 +42,15 @@ export function FormHome() {
           }
           if(!values.telephone){
             errors.telephone = 'Por favor, ingrese su télefono de contacto';
-          } else if(!/^[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(values.telephone)) {
+          } else if(!/^[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/.test(values.telephone)) {
             errors.telephone = 'El número telefónico solo puede contener números y guiones';
           }
           return errors;
         }} 
-        onSubmit={(values, { resetForm })=> {
-          resetForm();
-          //console.log(values); //Aqui va conexion a api o envio de datos
-          //Aqui recibimos los valores del cliente en un objeto
-          // podria revisar como enviar esta información a un correo elect.
-          setSendForm(true);
-          setTimeout(() => setSendForm(false), 10000);
-        }}
       >
 
-        {( {values, errors, touched, handleSubmit, handleChange, handleBlur} ) => (
-          <form onSubmit={SendEmail}>
+        {( {values, errors, touched, handleChange, handleBlur} ) => (
+          <form id='form' onSubmit={SendEmail}>
             <div>
               <label htmlFor="firstname">Nombre</label>
               <input 
@@ -114,79 +105,9 @@ export function FormHome() {
               {touched.telephone && errors.telephone && <div className='error'>{errors.telephone}</div>}
             </div>
             <button type="submit">Contactar a ventas</button>
-            {sendForm &&
-              <p className='success'>Confirmación de envío<br />Muchas gracias.<br />Pronto estaremos en contacto con usted.</p>
-            }
           </form>
         )}
       </Formik>
     </Fragment>
   );
 }
-
-/*
-{( {values, errors, touched, handleSubmit, handleChange, handleBlur} ) => (
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="firstname">Nombre</label>
-              <input 
-                type="text" 
-                id="firstname" 
-                name="firstname" 
-                value={values.firstname}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {touched.firstname && errors.firstname && <div className='error'>{errors.firstname}</div>}
-            </div>
-
-            <div>
-              <label htmlFor="lastname">Apellidos</label>
-              <input 
-                type="text" 
-                id="lastname" 
-                name="lastname" 
-                value={values.lastname}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {touched.lastname && errors.lastname && <div className='error'>{errors.lastname}</div>}
-            </div>
-
-            <div>
-              <label htmlFor="email">Correo electrónico</label>
-              <input 
-                type="email" 
-                id="email" 
-                name="email"
-                placeholder='usuario@correo.com'
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {touched.email && errors.email && <div className='error'>{errors.email}</div>}
-            </div>
-
-            <div>
-              <label htmlFor="telephone">Número telefónico</label>
-              <input
-                type="text"
-                id="telephone"
-                name="telephone"
-                placeholder='300-000-0000 o 300000000'
-                value={values.telephone}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {touched.telephone && errors.telephone && <div className='error'>{errors.telephone}</div>}
-            </div>
-            <button type="submit">Contactar a ventas</button>
-            {sendForm &&
-              <p className='success'>Confirmación de envío<br />Muchas gracias.<br />Pronto estaremos en contacto con usted.</p>
-            }
-          </form>
-        )}
-      </Formik>
-    </Fragment>
-  );
-} */
