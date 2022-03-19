9916fdc6973bba52b09957e8c10fd65f51ab21e1
dmhenaopa <dimhenaopa@gmail.com>
2022-03-19 14:11:09 -0500

    :construction_worker: Changes in build file - new version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { Navbar } from './components/Navbar/Navbar';
 import { Header } from './components/Header/Header';
 import { Home } from './pages/Home/Home';
@@ -21,13 +21,13 @@ function App() {
       <Navbar />
       <Header />
       <Routes>
-        <Route path='#/' element={<Home />} />
-        <Route path='#/productos' element={<Products />} />
-        <Route path='#/nosotros' element={<AboutUs />} />
-        <Route path='#/contacto' element={<Contact />} />
-        <Route path='#/login' element={<Login />} />
-        <Route path='#/user01/*' element={<User />} />
-        <Route path='#/admin/*' element={<Admin />} />
+        <Route path='/' element={<Home />} />
+        <Route exact path='/productos' element={<Products />} />
+        <Route exact path='/nosotros' element={<AboutUs />} />
+        <Route exact path='/contacto' element={<Contact />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route path='/user01/*' element={<User />} />
+        <Route path='/admin/*' element={<Admin />} />
       </Routes>
       <Feature />
       <Steps />
